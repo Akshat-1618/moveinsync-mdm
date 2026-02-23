b1e760fc8b0eff874d91a8b3e833570974f1bbb2
Akshat-1618 <akshatshyamsaxena760@gmail.com>
2026-02-23 20:19:33 +0530

    Cleanup Done

--- src/models/updateJob.model.js
+++ src/models/updateJob.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const updateJobSchema = new mongoose.Schema({
   target_version: { type: String, required: true },
-  region: String,              // optional targeting
+  region: String,              
   created_at: { type: Date, default: Date.now },
   status: { type: String, default: "scheduled" }
 });
